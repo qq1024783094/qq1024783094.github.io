26959976556f513782a6338095d2147379a29f18
qq1024783094 <1024783094@qq.com>
2025-01-14 00:36:18 +0800

    地塞米松

---
+++ assets/post.bz7Ns1Yi.js
@@ -0,0 +1,1 @@
+import{o as c}from"./app.HAp6JTgV.js";import{c as i}from"./framework.BDAG0DUs.js";function f(o){if(!o)return{color:"",icon:"",styles:void 0};const n=c();o in n.value.types||(o="link");const e=n.value.types[o].color,r=n.value.types[o].icon,s=i(()=>o?{"--card-c-primary":e}:void 0);return{color:e,icon:r,styles:s}}export{f as u};
