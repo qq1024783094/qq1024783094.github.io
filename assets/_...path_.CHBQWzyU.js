2a41ca20e3a4f6be35039007f193e8dd130c20bf
qq1024783094 <qq1024783094@users.noreply.github.com>
2026-01-20 02:13:36 +0000

    deploy: d6eaca4359add8591432b324c7f7196342661864

---
+++ assets/_...path_.CHBQWzyU.js
@@ -0,0 +1,1 @@
+import{u as a,a as s,d as t}from"./chunks/vue-router.BAde8S6R.js";import{w as n,a3 as r,ac as c,ab as p}from"./framework.BjA1SDoy.js";const u=n({__name:"[...path]",setup(f){const o=s(),e=a();return r(()=>{e.path!=="/"&&e.path.endsWith("/")&&o.replace(e.path.slice(0,-1))}),(i,m)=>(p(),c("div"))}});typeof t=="function"&&t(u);export{u as default};
