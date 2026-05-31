2d8817acc2c5a05bd9f698fcdbdcdbed1bd5af78
qq1024783094 <qq1024783094@users.noreply.github.com>
2026-05-31 09:32:55 +0000

    deploy: d1e1c59e37624cf81f6f2e9676516321564d2368

---
+++ assets/index2.BjmWWEoO.js
@@ -0,0 +1,1 @@
+import{W as e}from"./theme.DsIy-UTg.js";var t=e({defaults:{"@type":`AggregateRating`}});export{t as aggregateRatingResolver};
---
+++ assets/index28.DH7gmzji.js
@@ -0,0 +1,1 @@
+import{W as e}from"./theme.DsIy-UTg.js";var t=e({defaults:{"@type":`PostalAddress`}});export{t as addressResolver};
