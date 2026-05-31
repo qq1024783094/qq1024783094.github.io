915840ce988d0b7ba6b5a172aaf0868b742e47d0
qq1024783094 <1024783094@qq.com>
2026-05-31 17:55:59 +0800

    Fix posts avatar by creating custom YunPostMeta and removing conflicting image

---
+++ components/YunPostMeta.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { Post } from 'valaxy'
+import { useSiteConfig } from 'valaxy'
+
+const props = defineProps<{
+  post: Post
+}>()
+
+const site = useSiteConfig()
+</script>
+
+<template>
+  <div class="yun-post-meta flex gap-2 flex-wrap items-center">
+    <img
+      v-if="site.author.avatar"
+      :src="site.author.avatar"
+      :alt="site.author.name"
+      class="w-5 h-5 rounded-full"
+    >
+    <span class="text-sm">{{ site.author.name }}</span>
+    <YunMetaItem :date="post.date" :updated="post.updated" />
+    <YunMetaReadingTime :post="post" />
+    <YunMetaWordCount :post="post" />
+  </div>
+</template>
